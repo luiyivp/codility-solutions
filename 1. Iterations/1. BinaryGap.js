9af7a7f5b0c2b27af64ae9638140174c97ecf6e2
luiyivp <31863570+luiyivp@users.noreply.github.com>
2019-06-09 08:58:41 -0400

    1

---
+++ 1. Iterations/1. BinaryGap.js
@@ -0,0 +1,39 @@
+var binary = ''
+
+function solution(N) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    toBinary(N)
+    longestBinaryGap(binary)
+    return longestBinaryGap(binary)
+}
+
+function toBinary(num) {
+    if (num == 0) {
+        return binary
+    }
+    else {
+        if (num % 2 == 1) {
+            binary = '1' + binary
+            toBinary(~~(num / 2))
+        } else {
+            binary = '0' + binary
+            toBinary(~~(num / 2))
+        }
+    }
+}
+
+function longestBinaryGap(binary) {
+    var currentGap = 0
+    var maxGap = 0
+    for (var i = 0; i < binary.length; i++) {
+        if (binary.charAt(i) == 0) {
+            currentGap++
+        } else {
+            if (currentGap > maxGap) {
+                maxGap = currentGap
+                currentGap = 0
+            }
+        }
+    }
+    return maxGap
+}
